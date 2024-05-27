e5b8ddcd72907e4c981e5e8e71f5eb037a5ca31a
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2024-05-27 09:32:40 -0300

    Header Functionality
    Header menu opens and closes in small screens

---
+++ js/menu.js
@@ -0,0 +1,41 @@
+var all_items = document.getElementsByClassName("header__menu__item")
+
+var menu_is_open = 0
+
+function menu() {
+
+    var window_width = window.innerWidth
+
+    if (window_width > 850) {
+        return
+    } 
+
+    if (!menu_is_open) {
+        openMenu()
+    } else {
+        closeMenu()
+    }
+
+}
+
+function openMenu() {
+
+    for (item of all_items) {
+        item.style.display = "flex"
+    }
+
+    menu_is_open = 1
+
+    // document.getElementById("home-button-link::after").style.transform = "rotate(0deg)"
+}
+
+function closeMenu() {
+
+    for (item of all_items) {
+        item.style.display = "none"
+    }
+
+    document.getElementById("home-button").style.display = "flex"
+
+    menu_is_open = 0
+}
