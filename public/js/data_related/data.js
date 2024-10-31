89ed6ad814a1aa743429a7357892dee5ad697597
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2024-10-31 20:39:15 -0300

    Update data.js

--- public/js/data_related/data.js
+++ public/js/data_related/data.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 async function loadJSON() {
 
     // Gets the data of the JSON
-    const response = await fetch('/data.json')
+    const response = await fetch('https://gui27souza.github.io/message-flow-challenge/data.json')
     
     // Store data in global var
     data = await response.json()
